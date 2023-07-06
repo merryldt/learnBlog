8b323ddc505fe1cf97c2ce209fd8529ed5b38c0e
kansuper <17835059864@163.com>
2023-07-06 19:20:41 +0800

    整理

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -71,9 +71,9 @@ export default navbar([
         icon: "wechat",
         link: "https://img.newzone.top/wechat.svg",
       },
-      { text: "Email", icon: "alias", link: "mailto:learndata@newzone.top" },
-      { text: "Discord", icon: "group", link: "https://discord.gg/PZTQfJ4GjX" },
-      { text: "RSS", icon: "rss", link: "https://newzone.top/rss.xml" },
+      // { text: "Email", icon: "alias", link: "mailto:learndata@newzone.top" },
+      // { text: "Discord", icon: "group", link: "https://discord.gg/PZTQfJ4GjX" },
+      // { text: "RSS", icon: "rss", link: "https://newzone.top/rss.xml" },
     ],
   },
 ]);
