fbd4629aa2d26876b9078ddf8e86f2ebc2481954
kansuper <17835059864@163.com>
2023-07-10 20:10:12 +0800

    add

---
+++ docs/.vuepress/enhanceApp.js
@@ -0,0 +1,20 @@
+import ElementUI from 'element-ui';
+ 
+// 使用异步函数也是可以的
+export default ({
+  Vue, // VuePress 正在使用的 Vue 构造函数
+  options, // 附加到根实例的一些选项
+  router, // 当前应用的路由实例
+  siteData, // 站点元数据
+  isServer, // 当前应用配置是处于 服务端渲染 或 客户端
+}) => {
+  // ...做一些其他的应用级别的优化
+  // 全局注入组件
+  Vue.use(ElementUI);
+  Vue.mixin({
+    // 混合注入,加载全局文件
+    mounted() {
+      import('./public/js/btglunis');
+    },
+  });
+};
--- docs/DailyRoutine.md
+++ docs/DailyRoutine.md
@@ -15,7 +15,7 @@ icon: check
 你可能会说，怎么不是过呢，做呗？ 
 
 如果做的同时，可以做的更好，何乐而不为呢，学习安排自己合理的规则，还是有必要的。1111
-<HideArticle/>
+<!-- <HideArticle/>
 <script setup>
 import HideArticle from '../../compoment/HideArticle.vue'
-</script>
+</script> -->
