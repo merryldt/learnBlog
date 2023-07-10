e1802ba2cbf714f9dce1eb0531f16620e291031f
kansuper <17835059864@163.com>
2023-07-10 14:06:45 +0800

    add 隐藏

---
+++ docs/.vuepress/compoents/HideArticle.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+ 
+  </div>
+</template>
+ 
+<script>
+ const btwPlugin = require('../../public/js/btglunis');
+  export default {
+    name: 'HideArticle',
+    data() {
+      return {
+ 
+      }
+    },
+ 
+    mounted() {
+      btwPlugin();
+    }
+  }
+</script>
+ 
+<style lang="scss" scoped>
+ 
+</style>
+ 
